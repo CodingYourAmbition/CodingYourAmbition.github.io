1e0a2203b1e4747a5232dbeb4d5a8002f7b9701f
jhw <s1mpleocode@gmail.com>
2024-04-23 20:02:20 +0800

    添加站点运行时长、全站字数统计

--- themes/fluid/scripts/helpers/injects.js
+++ themes/fluid/scripts/helpers/injects.js
@@ -5,3 +5,7 @@
 hexo.extend.helper.register('point_injected', function(type) {
   return hexo.theme.config.injects[type] && hexo.theme.config.injects[type].length > 0;
 });
+
+hexo.extend.filter.register('theme_inject', function(injects) {
+  injects.bodyEnd.file('showword', 'source/_inject/show_wordcount_total.ejs', { key: 'value' }, -1);
+});
